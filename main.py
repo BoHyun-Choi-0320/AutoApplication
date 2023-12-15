5ece2da0a42a99286f9a75866ea09d3eff17d84d
BoHyun-Choi-0320 <320fleur@gmail.com>
2023-12-15 16:59:15 +0900

    update_23.12.15 - cell 테두리 적용

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
+from openpyxl.styles import Font, Border, Side
 from openpyxl.styles import numbers
 
 
@@ -32,7 +33,7 @@ class MyApp(QWidget):
         self.setLayout(grid)
 
         # 객체 생성
-        self.filePath = QLineEdit('E:/geostory/2023/타 부서 업무협조/해양사업부/데이터/자동화프로그램구축_총괄표_(신양식) 장애물 관리대장(무안공항)_231211.xlsx')
+        self.filePath = QLineEdit('E:/geostory/2023/타 부서 업무협조/해양사업부/데이터/총괄표/(신양식) 장애물 관리대장(무안공항)_231214.xlsx')
         self.folderPath = QLineEdit('E:/geostory/2023/타 부서 업무협조/해양사업부/데이터/FolderTree_v2')
         self.formPath = QLineEdit('E:/geostory/2023/타 부서 업무협조/해양사업부/데이터/장애물 관리대장 양식')
 
@@ -89,14 +90,15 @@ class MyApp(QWidget):
 
     # 관리대장 읽어오는 함수
     def readExcel(self):
-        self.pf = pd.read_excel(self.filePath.text(), header=3, usecols='B,C,E,I:L,P:AF,AJ,AL,AN,AP,AQ,AZ,BA,BH,BZ')
-        self.pf.columns = ['순번', '신규연도', '연번', '세부종류', '장애물 용도', '명칭', '건축주(소유주)', '특례 장애물 구분', '차폐 기준 장애물 및 지정일',
+        self.pf = pd.read_excel(self.filePath.text(), header=3, usecols='B,C,E,I:L,P:AF,AJ,AL,AN:AP,AZ:BA,BF,BY')
+        self.pf.columns = ['순번', '신규연도', '연번', '세부종류', '장애물 용도', '명칭', '건축주', '특례 장애물 구분', '차폐 기준 장애물 및 지정일',
                            '주소1', '주소2',
                            '주소3',
-                           '주소4', '도로명주소', '위치구역', '위도', '경도', 'X축', 'Y축', '지반높이(DEM)', '건물/시설물/수목 높이', '전체높이(DSM)',
+                           '주소4', '도로명주소', '위치구역', '위도', '경도', 'X축', 'Y축', '지반높이', '건물/시설물/수목 높이', '전체높이',
                            '제한표면',
                            '제한표면 침범높이',
-                           '협의높이', '위반여부', '기관명', '관리번호', '연락처', '건축허가일', '준공승인일', '장애물 등재일', '좌표/높이 결정방법']
+                           '협의높이', '위반여부', '기관명', '연락처', '관리번호', '건축허가일', '준공승인일', '장애물 등재일', '좌표/높이 결정방법']
+        print(self.pf)
         self.makeFolder()
         self.copyExcel()
 
@@ -165,24 +167,28 @@ class MyApp(QWidget):
             ws1['A9'].value =data['위도']
             ws1['B9'].value =data['경도']
             ws1['C9'].value =data['X축']
+            ws1['C9'].number_format = '00"˚"00"′"00.00"″"'
             ws1['D9'].value =data['Y축']
+            ws1['D9'].number_format = '00"˚"00"′"00.00"″"'
             ws1['E9'].value =data['좌표/높이 결정방법']
             ws1['F9'].value =data['건축허가일']
+            ws1['F9'].number_format = "yy/mm/dd"
             ws1['G9'].value =data['준공승인일']
-            ws1['A13'].value =data['지반높이(DEM)']
+            ws1['G9'].number_format = "yy/mm/dd"
+            ws1['A13'].value =data['지반높이']
             ws1['B13'].value =data['건물/시설물/수목 높이']
-            ws1['C13'].value =data['전체높이(DSM)']
+            ws1['C13'].value =data['전체높이']
             ws1['D13'].value =data['제한표면']
             ws1['E13'].value =data['제한표면 침범높이']
             ws1['F13'].value =data['협의높이']
             ws1['G13'].value =data['위반여부']
             ws1['A18'].value =data['신규연도']
-            ws1['A18'].number_format = 'yy/mm/dd'
+            ws1['A18'].number_format = "yy/mm/dd"
             ws1['B18'].value =data['특례 장애물 구분']
             ws1['C18'].value =data['차폐 기준 장애물 및 지정일']
             ws1['E18'].value =data['장애물 등재일']
             ws1['A23'].value =data['장애물 용도']
-            ws1['B23'].value =data['건축주(소유주)']
+            ws1['B23'].value =data['건축주']
             ws1['C23'].value =data['기관명']
             ws1['D23'].value =data['연락처']
             ws1['F23'].value =data['관리번호']
@@ -190,9 +196,17 @@ class MyApp(QWidget):
             if data['순번'] != '제거':
                 self.typeImage(data['세부종류'], imgPath, data, wb, ws1, ws2, savePath)
 
+            ws1['G20'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
+            ws1['G21'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
+            ws1['G22'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
+            ws1['G23'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
+            ws1['G24'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
+            ws1.font = Font(name='돋움', size=11)
             wb.save(savePath)
         except Exception as e:
             print("inputDataToExcel() - Error! : ", e)
+
+
 
     def typeImage(self, type, imgPath, data,wb, ws1, ws2, savePath):
         print('MyApp - typeImage()')
