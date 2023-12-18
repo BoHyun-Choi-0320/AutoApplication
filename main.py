f2361f809be167f9e31925f6fcfef0c5f51e7c63
BoHyun-Choi-0320 <320fleur@gmail.com>
2023-12-18 17:58:04 +0900

    update_23.12.18 - 서식 및 pdf 저장

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import shutil
 import sys
+
+import PyPDF2
+import openpyxl.styles
 import pandas as pd
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
 from openpyxl.styles import Font, Border, Side
-from openpyxl.styles import numbers
+import xlwings as xw
+import win32com.client as win32
+from PyPDF2 import PdfMerger, PdfReader
 
 
 class MyApp(QWidget):
@@ -106,12 +111,19 @@ class MyApp(QWidget):
     def makeFolder(self):
         print('MyApp - makeFolder()')
         self.path = os.getcwd()
-        self.savePath = self.path + "/result"
+        self.savePath = self.path + "/result/excel"
         try:
             if not os.path.exists(self.savePath):
                 os.mkdir(self.savePath)
         except:
             print('Error : Creating directory.' + self.savePath)
+
+        self.pdfPath = os.path.dirname(self.savePath) + "/pdf"
+        try:
+            if not os.path.exists(self.pdfPath):
+                os.mkdir(self.pdfPath)
+        except:
+            print('Error : Creating directory.' + self.pdfPath)
 
     def copyExcel(self):
         print('MyApp - copyExcel()')
@@ -119,7 +131,7 @@ class MyApp(QWidget):
         try:
             for i in range(len(self.pf)):
                 imgPath = self.folderPath.text() + "/" + str(self.pf['연번'][i])
-                print('imgPath : ',imgPath)
+                print('imgPath : ', imgPath)
                 if self.pf['세부종류'][i] == '나무':
                     fileName = '/' + str(self.pf['연번'][i]) + ".xlsx"
                     save = self.savePath + fileName
@@ -156,62 +168,86 @@ class MyApp(QWidget):
             ws1 = wb["연번"]
             ws2 = wb["연번(장애물 관리대장 상세표)"]
             ws1.title = str(data['연번'])
-            ws2.title = str(data['연번'])+"(장애물 관리대장 상세표)"
-            print("sheet 이름", wb.sheetnames)
+            ws2.title = str(data['연번']) + "(장애물 관리대장 상세표)"
 
-            ws1['A5'].value=data['연번']
-            ws1['B5'].value=data['명칭']
-            ws1['C5'].value =data['주소1'] + " " + data['주소2'] + " " + data['주소3'] + " " + data['주소4']
-            ws1['D6'].value =data['도로명주소']
-            ws1['F5'].value =data['위치구역']
-            ws1['A9'].value =data['위도']
-            ws1['B9'].value =data['경도']
-            ws1['C9'].value =data['X축']
+            ws1['A5'].value = data['연번']
+            ws1['B5'].value = data['명칭']
+            ws1['C5'].value = data['주소1'] + " " + data['주소2'] + " " + data['주소3'] + " " + data['주소4']
+            ws1['D6'].value = data['도로명주소']
+            ws1['F5'].value = data['위치구역']
+            ws1['A9'].value = data['위도']
+            ws1['B9'].value = data['경도']
+            ws1['C9'].value = data['X축']
             ws1['C9'].number_format = '00"˚"00"′"00.00"″"'
-            ws1['D9'].value =data['Y축']
+            ws1['D9'].value = data['Y축']
             ws1['D9'].number_format = '00"˚"00"′"00.00"″"'
-            ws1['E9'].value =data['좌표/높이 결정방법']
-            ws1['F9'].value =data['건축허가일']
-            ws1['F9'].number_format = "yy/mm/dd"
-            ws1['G9'].value =data['준공승인일']
-            ws1['G9'].number_format = "yy/mm/dd"
-            ws1['A13'].value =data['지반높이']
-            ws1['B13'].value =data['건물/시설물/수목 높이']
-            ws1['C13'].value =data['전체높이']
-            ws1['D13'].value =data['제한표면']
-            ws1['E13'].value =data['제한표면 침범높이']
-            ws1['F13'].value =data['협의높이']
-            ws1['G13'].value =data['위반여부']
-            ws1['A18'].value =data['신규연도']
-            ws1['A18'].number_format = "yy/mm/dd"
-            ws1['B18'].value =data['특례 장애물 구분']
-            ws1['C18'].value =data['차폐 기준 장애물 및 지정일']
-            ws1['E18'].value =data['장애물 등재일']
-            ws1['A23'].value =data['장애물 용도']
-            ws1['B23'].value =data['건축주']
-            ws1['C23'].value =data['기관명']
-            ws1['D23'].value =data['연락처']
-            ws1['F23'].value =data['관리번호']
+            ws1['E9'].value = data['좌표/높이 결정방법']
+            ws1['F9'].value = data['건축허가일']
+            if type(data['건축허가일']) == str:
+                ws1['F9'].value = data['건축허가일']
+            else:
+                ws1['F9'].value = data['건축허가일'].strftime('%y/%m/%d')
+
+            if type(data['준공승인일']) == str:
+                ws1['G9'].value = data['준공승인일']
+            else:
+                ws1['G9'].value = data['준공승인일'].strftime('%y/%m/%d')
+
+            ws1['A13'].value = data['지반높이']
+            ws1['B13'].value = data['건물/시설물/수목 높이']
+            ws1['C13'].value = data['전체높이']
+            ws1['D13'].value = data['제한표면']
+            ws1['E13'].value = data['제한표면 침범높이']
+            ws1['F13'].value = data['협의높이']
+            ws1['G13'].value = data['위반여부']
+            if type(data['신규연도']) == str:
+                ws1['A18'].value = data['신규연도']
+            else:
+                ws1['A18'].value = data['신규연도'].strftime('%y/%m/%d')
+
+            ws1['B18'].value = data['특례 장애물 구분']
+            ws1['C18'].value = data['차폐 기준 장애물 및 지정일']
+            ws1['E18'].value = data['장애물 등재일']
+            ws1['A23'].value = data['장애물 용도']
+            ws1['B23'].value = data['건축주']
+            ws1['C23'].value = data['기관명']
+            ws1['D23'].value = data['연락처']
+            ws1['F23'].value = data['관리번호']
 
             if data['순번'] != '제거':
                 self.typeImage(data['세부종류'], imgPath, data, wb, ws1, ws2, savePath)
 
-            ws1['G20'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
-            ws1['G21'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
-            ws1['G22'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
-            ws1['G23'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
-            ws1['G24'].border = Border(right=Side(border_style='medium', color="000000"),bottom=Side(border_style='thin', color="000000"),left=Side(border_style='thin', color="000000"),top=Side(border_style='thin', color="000000"))
+            ws1['G20'].border = Border(right=Side(border_style='medium', color="000000"),
+                                       bottom=Side(border_style='thin', color="000000"),
+                                       left=Side(border_style='thin', color="000000"),
+                                       top=Side(border_style='thin', color="000000"))
+            ws1['G21'].border = Border(right=Side(border_style='medium', color="000000"),
+                                       bottom=Side(border_style='thin', color="000000"),
+                                       left=Side(border_style='thin', color="000000"),
+                                       top=Side(border_style='thin', color="000000"))
+            ws1['G22'].border = Border(right=Side(border_style='medium', color="000000"),
+                                       bottom=Side(border_style='thin', color="000000"),
+                                       left=Side(border_style='thin', color="000000"),
+                                       top=Side(border_style='thin', color="000000"))
+            ws1['G23'].border = Border(right=Side(border_style='medium', color="000000"),
+                                       bottom=Side(border_style='thin', color="000000"),
+                                       left=Side(border_style='thin', color="000000"),
+                                       top=Side(border_style='thin', color="000000"))
+            ws1['G24'].border = Border(right=Side(border_style='medium', color="000000"),
+                                       bottom=Side(border_style='thin', color="000000"),
+                                       left=Side(border_style='thin', color="000000"),
+                                       top=Side(border_style='thin', color="000000"))
             ws1.font = Font(name='돋움', size=11)
             wb.save(savePath)
+
+            self.excelToPDF(savePath, str(data['연번']))
         except Exception as e:
             print("inputDataToExcel() - Error! : ", e)
 
-
-
-    def typeImage(self, type, imgPath, data,wb, ws1, ws2, savePath):
+    def typeImage(self, type, imgPath, data, wb, ws1, ws2, savePath):
         print('MyApp - typeImage()')
         try:
-            self.setImage(wb=wb,ws=ws1, width=757.23, height=426.27, imgPath=imgPath, data=data, position="A25",
+            self.setImage(wb=wb, ws=ws1, width=21.41, height=11.28, imgPath=imgPath, data=data, position="A25",
                           imgType="/현장사진_", savePath=savePath)
             if type == '나무':
                 self.setImage(wb=wb, ws=ws2, width=738.62, height=434.17, imgPath=imgPath, data=data,
@@ -247,10 +283,43 @@ class MyApp(QWidget):
             imgPath = imgPath + imgType + str(data['연번']) + ".jpg"
             img = Image(imgPath)
             ws.add_image(img, position)
-            img.width, img.height = width, height
+            img.width, img.height = self.get_col_width_row_height(width, height)
             wb.save(savePath)
         except Exception as e:
-            print("setImage - Error : ",imgType," 이미지가 없습니다.", e)
+            print("setImage - Error : ", imgType, " 이미지가 없습니다.", e)
+
+    def get_col_width_row_height(self, img_width, img_height):
+        col_width = (img_width * 7300) / 193 - 10
+        row_height = (img_height * 7300) / 193 - 5
+        return (col_width, row_height)
+
+    def excelToPDF(self, excelPath, filename):
+        print('MyApp - excelToPDF()')
+        try:
+            app = xw.App(visible=False)
+            book = xw.Book(excelPath)
+            print(self.pdfPath + "/" + filename + "_1.pdf")
+            pdf1 = self.pdfPath + "/" + filename + "_1.pdf"
+            pdf2 = self.pdfPath + "/" + filename + "_2.pdf"
+            book.sheets[0].api.ExportAsFixedFormat(0, pdf1)
+            book.sheets[1].api.ExportAsFixedFormat(0, pdf2)
+            app.kill()
+
+            pdfMerger = PdfMerger()
+
+            pdfMerger.append(PdfReader(open(pdf1,"rb")))
+            pdfMerger.append(PdfReader(open(pdf2, "rb")))
+
+            pdfMerger.write(self.pdfPath+"/"+filename+".pdf")
+            print(pdf1, pdf2)
+
+            if os.path.isfile(pdf1):
+                os.remove(pdf1)
+
+            if os.path.isfile(pdf2):
+                os.remove(pdf2)
+        except Exception as e:
+            print('excelToPDF Error : '+e)
 
 
 if __name__ == '__main__':
